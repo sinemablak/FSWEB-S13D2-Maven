1d1c693791e7656832728c767931fe4c86326305
sinemablak <sinemablakk1@gmail.com>
2024-07-31 22:29:15 +0300

    polindrom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,32 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(-11212));
     }
+
+    public static boolean isPalindrome(int number) {
+        number=Math.abs(number);
+        String str=String.valueOf(number);
+        int left=0;
+        int rigth=str.length()-1;
+
+        while(left<rigth){
+            if(str.charAt(left)!=str.charAt(rigth)){
+                return false;
+            }
+            left++;
+            rigth--;
+        }
+        return true;
+
+
+
+
+    }
+
+
 }
+
+
